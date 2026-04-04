51546351aa0a9e8e26717fd42b1783ebb2ba62c0
Sophia <sophiamunizdeoliveira2006@gmail.com>
2026-04-04 00:42:33 +0000

    questoes

--- ed/triangulo/go/main.go
+++ ed/triangulo/go/main.go
@@ -9,11 +9,21 @@ import (
 )
 
 func processa(vet []int) {
-	_ = vet;
 	// 1. defina o ponto de parada
+	if len(vet) == 1 {
+		fmt.Println("[ " + Join(vet, " ") + " ]")
+		return
+	}
 	// 2. monte o vetor auxiliar com os resultados das somas
+	aux := []int{}
+	for i := 0; i < len(vet)-1; i++ {
+		aux = append(aux, vet[i]+vet[i+1])
+	}
 	// 3. chame recursivamente a função processa para o vetor auxiliar
+	processa(aux)
+
 	// 4. imprima o vetor original
+	fmt.Println("[ " + Join(vet, " ") + " ]")
 }
 
 func main() {
