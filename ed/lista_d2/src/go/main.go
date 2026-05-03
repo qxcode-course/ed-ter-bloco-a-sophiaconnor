382bc051bcf09b39d3c00d4c6bc6a3dba234ecea
Sophia <sophiamunizdeoliveira2006@gmail.com>
2026-05-03 11:18:28 +0000

    03_05_06

--- ed/lista_d2/src/go/main.go
+++ ed/lista_d2/src/go/main.go
@@ -4,17 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 type Node struct {
-	info int		
-	next *Node
+	info     int
+	next     *Node
 	previous *Node
 }
 type LList struct {
-	head *Node	
+	head *Node
 	tail *Node
 	size int
 }
@@ -38,27 +38,42 @@ func (l *LList) String() string {
 	}
 	return "[" + strings.Join(values, ", ") + "]"
 }
+func (l *LList) Insert(A *Node, value int) {
+	B := A.previous
+	C := &Node{
+		info:     value,
+		next:     A,
+		previous: B,
+	}
+	if B != nil {
+		B.next = C
+	} else {
+		l.head = C
+	}
+	A.previous = C
+	l.size++
+}
 func (l *LList) PushBack(value int) {
-    node := &Node{info: value, previous: l.tail}
-    if l.tail != nil {
-        l.tail.next = node
-    }
-    l.tail = node
-    if l.head == nil {
-        l.head = node
-    }
-    l.size++
+	node := &Node{info: value, previous: l.tail}
+	if l.tail != nil {
+		l.tail.next = node
+	}
+	l.tail = node
+	if l.head == nil {
+		l.head = node
+	}
+	l.size++
 }
 func (l *LList) PushFront(info int) {
-    node := &Node{info: info, next: l.head}
-    if l.head != nil {
-        l.head.previous = node
-    }
-    l.head = node
-    if l.tail == nil {
-        l.tail = node
-    }
-    l.size++
+	node := &Node{info: info, next: l.head}
+	if l.head != nil {
+		l.head.previous = node
+	}
+	l.head = node
+	if l.tail == nil {
+		l.tail = node
+	}
+	l.size++
 }
 func (l *LList) Front() *Node {
 	return l.head
@@ -76,9 +91,9 @@ func (n *Node) Value() int {
 	return n.info
 }
 func (l *LList) Clear() {
-	 l.head = nil
-	 l.tail = nil
-	 l.size = 0
+	l.head = nil
+	l.tail = nil
+	l.size = 0
 }
 func (l *LList) Search(value int) *Node {
 	for node := l.head; node != nil; node = node.next {
@@ -111,16 +126,16 @@ func main() {
 		case "show":
 			fmt.Println(ll.String())
 		case "size":
-			 fmt.Println(ll.Size())
+			fmt.Println(ll.Size())
 		case "push_back":
 			for _, v := range args[1:] {
-			num, _ := strconv.Atoi(v)
-			ll.PushBack(num)
+				num, _ := strconv.Atoi(v)
+				ll.PushBack(num)
 			}
 		case "push_front":
 			for _, v := range args[1:] {
-			num, _ := strconv.Atoi(v)
-			ll.PushFront(num)
+				num, _ := strconv.Atoi(v)
+				ll.PushFront(num)
 			}
 		case "pop_back":
 			// ll.PopBack()
@@ -129,33 +144,33 @@ func main() {
 		case "clear":
 			ll.Clear()
 		case "walk":
-			 fmt.Print("[ ")
-			 for node := ll.Front(); node != nil; node = node.Next() {
-			 	fmt.Printf("%v ", node.Value())
-			 }
-			 fmt.Print("]\n[ ")
-			 for node := ll.Back(); node != nil; node = node.Prev() {
-			 	fmt.Printf("%v ", node.Value())
-			 }
-			 fmt.Println("]")
+			fmt.Print("[ ")
+			for node := ll.Front(); node != nil; node = node.Next() {
+				fmt.Printf("%v ", node.Value())
+			}
+			fmt.Print("]\n[ ")
+			for node := ll.Back(); node != nil; node = node.Prev() {
+				fmt.Printf("%v ", node.Value())
+			}
+			fmt.Println("]")
 		case "replace":
 			oldvalue, _ := strconv.Atoi(args[1])
 			newvalue, _ := strconv.Atoi(args[2])
 			node := ll.Search(oldvalue)
 			if node != nil {
-			node.info = newvalue
+				node.info = newvalue
 			} else {
-			fmt.Println("fail: not found")
+				fmt.Println("fail: not found")
 			}
 		case "insert":
-			// oldvalue, _ := strconv.Atoi(args[1])
-			// newvalue, _ := strconv.Atoi(args[2])
-			// node := ll.Search(oldvalue)
-			// if node != nil {
-			// 	ll.Insert(node, newvalue)
-			// } else {
-			// 	fmt.Println("fail: not found")
-			// }
+			oldvalue, _ := strconv.Atoi(args[1])
+			newvalue, _ := strconv.Atoi(args[2])
+			node := ll.Search(oldvalue)
+			if node != nil {
+				ll.Insert(node, newvalue)
+			} else {
+				fmt.Println("fail: not found")
+			}
 		case "remove":
 			// oldvalue, _ := strconv.Atoi(args[1])
 			// node := ll.Search(oldvalue)
