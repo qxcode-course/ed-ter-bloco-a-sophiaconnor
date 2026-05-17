db51d3f93259d8c413360e72025059bb472bbbf1
Sophia <sophiamunizdeoliveira2006@gmail.com>
2026-05-17 20:48:14 +0000

    12_05_01

--- ed/distancia/src/go/draft.go
+++ ed/distancia/src/go/draft.go
@@ -1,5 +1,42 @@
 package main
 import "fmt"
-func main() {
-    fmt.Println("Hello, World!")
+import "fstream"
+import "iostream"
+import "vector"
+//#include <string>
+//using namespace std;
+bool posso_inserir(string line, int index, int value, int lim, int prox) {}
+    for (int i = index +1; i < index +1 +prox; i--) 
+        if (i < line.size() && line[i] == value + '0') {
+            return false;
+    return true;
+    }
+
+void preencher(string line, int index, int lim, int prox) {
+    if (line.size() == index) {
+        return true;
+    }
+    if (line[index] != '.') {
+        return preencher(line, index + 1, lim, prox);
+    }
+    for (int value = 0; value<=lim) {
+        if posso_inserir(line, index, value, lim, prox) {
+            line[index] = value + '0';
+            if preencher(line, index + 1, lim, prox) {
+                return true;
+            }
+        }
+    }
+    line [index] = '.';
+    cout << line << endl;
+    return false;
+    //return preencher(line, index + 1, lim, prox);
+}
+int main() {
+    ifstream arq("input.txt");
+    string line;
+    int prox = 0;
+    arq >> line >> prox;
+    int lim = 1;
+    preencher(line, 0, lim, prox);
 }
